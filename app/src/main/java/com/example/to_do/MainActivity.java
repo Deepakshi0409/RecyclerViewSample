d2eb11d5f2b42c70bd9192ce2d79b63740591adb
user <Deepakshi.deeps@gmail.com>
2020-06-10 11:49:26 +0530

    to_do

--- app/src/main/java/com/example/to_do/MainActivity.java
+++ app/src/main/java/com/example/to_do/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.to_do;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
@@ -11,15 +12,14 @@ import android.widget.EditText;
 public class MainActivity extends AppCompatActivity {
     private EditText et;
     private Button btn;
-    private RecyclerView rcv;
+    private RecyclerView mNumbersList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-    et = findViewById(R.id. item_edit_text);
-    btn = findViewById(R.id.add_new_list);
-    rcv = findViewById(R.id.rv_items);
+        RecyclerView mNumbersList = findViewById(R.id.rv_items);
+        mNumbersList.setLayoutManager(new LinearLayoutManager(this));
     }
 }
