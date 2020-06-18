e45e9de620941fc7691e7974a0a7bec75abe0549
user <Deepakshi.deeps@gmail.com>
2020-06-18 13:15:48 +0530

    to_do

--- app/src/main/java/com/example/to_do/MyListAdapter.java
+++ app/src/main/java/com/example/to_do/MyListAdapter.java
@@ -1,17 +1,16 @@
 package com.example.to_do;
 
+import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.recyclerview.widget.RecyclerView;
 
-public class MyListAdapter extends RecyclerView.Adapter<MyListAdapter.NumberViewHolder> {
-    private String mItemsList;
-    public MyListAdapter(String numberOfItems) {
-        mItemsList = numberOfItems;
-    }
-
-    }
 
 
--- app/src/main/java/com/example/to_do/MyListData.java
+++ app/src/main/java/com/example/to_do/MyListData.java
@@ -1,41 +1,15 @@
 package com.example.to_do;
 
 public class MyListData {
-    public static String getViewHolderData(String instanceNum) {
-        switch (instanceNum) {
-            case "a":
-                return "Send an E mail" ;
-            case "b":
-                return "Pay bills";
-            case "c":
-                return "Attend function";
-            case "d":
-                return "Make calls";
-            case "e":
-                return "Complete presentation";
-            case "f":
-                return "Follow up";
-            case "g":
-                return "task 1";
-            case "h":
-                return "task 2";
-            case "i":
-                return "task 3";
-            case "j":
-                return "task 4";
-            case "k":
-                return "task 5";
-            case "l":
-                return "task 6";
-            case "m":
-                return "task 7";
-            case "n":
-                return "task 8";
-            case "o":
-                return "task 9";
-            default:
-                return "nil";
-        }
+    private String description;
+    public MyListData(String description, int imgId) {
+        this.description = description;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
     }
 
 }
